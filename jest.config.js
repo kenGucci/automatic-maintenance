104018e4be0240eb796fe42a63298796874fe917
OX-gucci :] <kencarsin665@gmail.com>
2026-06-02 08:03:46 +0100

    add: Jest testing configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!node_modules/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  testTimeout: 10000,
+  verbose: true,
+};
